18289f62e26db636db5be07439d5ab27da6da5b4
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-25 17:06:29 -0500

    added approval card comp

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = ({ author, timeAgo, content, avatar }) => {
     return (
         <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
+          <img alt="avatar" src={avatar} />
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
+            {author}
           </a>
           <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
+            <span className="date">{timeAgo}</span>
           </div>
-          <div className="text">Nice blog post!</div>
+          <div className="text">{content}</div>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={Faker.internet.avatar()}
+          />
+      </ApprovalCard>
     </div>
   );
 };
